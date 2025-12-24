b6b1f12fa50e22142a23736209dcb04ddb2c46b2
gladcow <jahr@yandex.ru>
2025-12-24 17:59:33 +0100

    remove repetition

--- main.py
+++ main.py
@@ -27,36 +27,35 @@ def main():
     initial_balance = 100.0
     finder = MarketFinder(GAMMA_URL)
 
-    while True:
-        print("Current 15 min BTC market:")
-        market_id = finder.get_current_market_id()
-        start = finder.get_current_slot_start()
-        market = Market(CLOB_URL, PK, FUNDER, CHAIN_ID, market_id, DRY_MODE)
-        info = market.market_info() # TODO: check market is active
-        print(info)
+    print("Current 15 min BTC market:")
+    market_id = finder.get_current_market_id()
+    start = finder.get_current_slot_start()
+    market = Market(CLOB_URL, PK, FUNDER, CHAIN_ID, market_id, DRY_MODE)
+    info = market.market_info() # TODO: check market is active
+    print(info)
 
-        strategy = TradeStrategy(market, ORDER_SIZE, MAX_INIT_COMBINED_PRICE, PAIR_DIFFERENCE_THRESHOLD)
-        while finder.slot_is_active(start):
-            if strategy.init():
-                break
-            time.sleep(INIT_INTERVAL)
-        # main loop
-        while finder.slot_is_active(start):
-            strategy.trade()
-            if strategy.current_profit() > TAKE_PROFIT_THRESHOLD:
-                print("Take profit")
-                break
-            time.sleep(TRADE_INTERVAL)
-            print(f"Current Pair Cost: {strategy.average_pair_cost()}")
+    strategy = TradeStrategy(market, ORDER_SIZE, MAX_INIT_COMBINED_PRICE, PAIR_DIFFERENCE_THRESHOLD)
+    while finder.slot_is_active(start):
+        if strategy.init():
+            break
+        time.sleep(INIT_INTERVAL)
+    # main loop
+    while finder.slot_is_active(start):
+        strategy.trade()
+        if strategy.current_profit() > TAKE_PROFIT_THRESHOLD:
+            print("Take profit")
+            break
+        time.sleep(TRADE_INTERVAL)
+        print(f"Current Pair Cost: {strategy.average_pair_cost()}")
 
-        print(f"Spent: {strategy.spent()}")
-        print(f"Profit for Up: {strategy.up_profit()}")
-        print(f"Profit for Down: {strategy.down_profit()}")
-        info = market.market_info()
-        print(info)
-        if finder.slot_is_active(start):
-            print("Wait for the next slot")
-            finder.wait_until_next_slot_start(start)
+    print(f"Spent: {strategy.spent()}")
+    print(f"Profit for Up: {strategy.up_profit()}")
+    print(f"Profit for Down: {strategy.down_profit()}")
+    info = market.market_info()
+    print(info)
+    if finder.slot_is_active(start):
+        print("Wait for the next slot")
+        finder.wait_until_next_slot_start(start)
 
 
 if __name__ == "__main__":
